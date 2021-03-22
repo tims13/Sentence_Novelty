e7a2f0c90bed7bb13ceef17a0b819924fa892bc2
tim <405362140@qq.com>
2021-03-22 13:42:12 +0800

    evaluate_novel

--- evaluate.py
+++ evaluate.py
@@ -3,16 +3,27 @@ import torch
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, recall_score, precision_score
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.ensemble import IsolationForest
 from sklearn.svm import OneClassSVM
 import seaborn as sns
 
+def evaluate_novel(model, train_features, test_features, y_true, name):
+    model.fit(train_features)
+    y_pred = model.predict(test_features)
+    y_pred[y_pred != -1] = 0
+    y_pred[y_pred == -1] = 1
+    recall_score = recall_score(y_true, y_pred)
+    prec_score = precision_score(y_true, y_pred)
+    print(name + ' RECALL:' + str(recall_score))
+    print(name + ' PRECISION:' + str(prec_score))
+
+
 def evaluate(model, train_loader, test_loader, novel_loader, device, des_folder, threshold=0.5):
     y_pred = []
     y_true = []
-
+    y_novel = []
     model.eval()
     train_features = []
     test_features = []
@@ -45,6 +56,7 @@ def evaluate(model, train_loader, test_loader, novel_loader, device, des_folder,
                 text_len = text_len.to(device)
                 output = model(text, text_len)
                 novel_features.append(model.deep_features.cpu().numpy())
+                y_novel.extend(labels.tolist())
 
     novel_feature_num = len(novel_features)
     test_features = np.vstack(test_features)
@@ -66,34 +78,20 @@ def evaluate(model, train_loader, test_loader, novel_loader, device, des_folder,
     plt.savefig(des_folder + '/eval.png')
 
     novel_test_features = np.vstack((test_features, novel_features))
+    y_true.extend(y_novel)
+    # 0 for not novel, 1 for novel
+    y_true[y_true != 2] = 0
+    y_true[y_true == 2] = 1
     # novel detect, fetch as many as possible
     print('LOF training...')
     lof = LocalOutlierFactor(n_neighbors=20, contamination=0.5, novelty=True, n_jobs=-1)
-    lof.fit(train_features)
-    y_pred_lof = lof.predict(novel_test_features)
-    novel_num = sum(y_pred_lof == -1)
-    lof_recall = novel_num / novel_feature_num
-    lof_prec = novel_num / len(y_pred_lof)
-    print('LOF RECALL:' + str(lof_recall))
-    print('LOF PRECISION:' + str(lof_prec))
+    evaluate_novel(lof, train_features, novel_test_features, y_true, 'LOF')
 
     print('Isolation Forest training...')
     isolation_forest = IsolationForest(random_state=0, contamination=0.5, n_jobs=-1)
-    isolation_forest.fit(train_features)
-    y_pred_iso = isolation_forest.predict(novel_features)
-    novel_num = sum(y_pred_iso == -1)
-    iso_recall = novel_num / novel_feature_num
-    iso_prec = novel_num / len(y_pred_iso)
-    print('ISOLATION FOREST RECALL:' + str(iso_recall))
-    print('ISOLATION FOREST PRECISION:' + str(iso_prec))
+    evaluate_novel(isolation_forest, train_features, novel_test_features, y_true, 'ISO')
 
     print('OC-SVM training...')
     oc_svm = OneClassSVM(gamma='auto')
-    oc_svm.fit(train_features)
-    y_pred_oc = oc_svm.predict(novel_features)
-    novel_num = sum(y_pred_oc == -1)
-    oc_recall = novel_num / novel_feature_num
-    oc_prec = novel_num / len(y_pred_oc)
-    print('OC-SVM RECALL:' + str(oc_recall))
-    print('OC-SVM PRECISION:' + str(oc_prec))
+    evaluate_novel(oc_svm, train_features, novel_test_features, y_true, 'OC-SVM')
     
